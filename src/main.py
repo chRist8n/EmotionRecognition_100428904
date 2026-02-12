04fe8aca702e370e638e0674ba185c7552d303bb
chRist8n <152877601+chRist8n@users.noreply.github.com>
2026-02-12 12:34:25 +0000

    Basic image processing
    greyscale, resize to 265x265, save to "processed" folder

--- src/main.py
+++ src/main.py
@@ -1,5 +1,42 @@
+from PIL import Image
+import os
+
+
 def main():
     NotImplemented
 
+def normalize_image(input_path, output_path, size=(256, 256)):
+    """
+    Loads image, converts to grayscale, resizes, saves result.
+    """
+
+    # load image
+    img = Image.open(input_path)
+
+    # convert to grayscale
+    img = img.convert("L")
+
+    # resize
+    img = img.resize(size)
+
+    # ensure output folder exists
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # save
+    img.save(output_path)
+
+    return output_path
+
+
 if __name__ == "__main__":
-    main()
+    test_input = "data/raw/RAF_DB/DATASET/train/1/train_00010_aligned.jpg" #"../../data/raw/RAF_DB/DATASET/test.jpg"
+    test_output = "data/processed/test_normalized.jpg"
+
+    result = normalize_image(test_input, test_output)
+    print("Saved to:", result)
+
+
+
+
+#if __name__ == "__main__":
+#    main()
