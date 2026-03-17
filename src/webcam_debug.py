ab63a568cb8719d2665844a35d92b5a8350649c5
chRist8n <152877601+chRist8n@users.noreply.github.com>
2026-03-17 16:13:13 +0000

    Webcam debug loop works
    function sare imported and will be reloaded every 10 frames so it can run while coding
    no functions are implemented on it yet, this version is just the plain webcam for backup purposes

--- src/webcam_debug.py
+++ src/webcam_debug.py
@@ -1,31 +1,49 @@
 """
 This file runs a loop to display a live webcam feed which 
 has been fedthrough the algorithm(s) for debugging purposes.
+-   Uses importlib.reload() to update file changes while
+    debugging
 -   Quit with [`] key
 """
 from preprocess import preprocess_image
 from detection.face_detector import detect_faces
 import cv2
+import importlib
 
 
 # open webcam (0 = default camera)
 cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read() 
-    if not ret:         # ret will be true if frame captured successfully
-        break
+try:
+    frame_count = 0
+    while True:
+        #reload all imported files from this project every 10 frames
+        frame_count += 1
+        if frame_count % 10 == 0:
+            importlib.reload(preprocess_image)
+            importlib.reload(detect_faces)
+            frame_count = 0
 
-    #preprocess
+        #capture frame
+        ret, frame = cap.read() 
+        if not ret:         # ret will be true if frame captured successfully
+            break
 
-    #detect face(s)
+        #preprocess
 
-    #display frame
-    cv2.imshow("Webcam", frame)
+        #detect face(s)
 
-    #exit with [`] key
-    if cv2.waitKey(1) & 0xFF == ord('`'):
-        break
+        #display frame
+        cv2.imshow("Webcam", frame)
 
-cap.release()
-cv2.destroyAllWindows()
+        #exit with [`] key
+        if cv2.waitKey(1) & 0xFF == ord('`'):
+            break
+except Exception as e:
+    #incase of actual exception, print to console
+    print(f"Error: \n, {e}" )
+finally:
+    #always release and destroy everything on close
+    cap.release()
+    cv2.destroyAllWindows()
+    
