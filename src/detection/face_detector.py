b3ba9a592fcc86d4690e275260a03cfbd3b2d060
chRist8n <152877601+chRist8n@users.noreply.github.com>
2026-03-18 11:34:03 +0000

    some more debugging implemented
    + some minor changes

--- src/detection/face_detector.py
+++ src/detection/face_detector.py
@@ -1,25 +1,29 @@
 from detection.integral_image import compute_integral_image
-from detection.haar_features import haar_feature_horizontal
+from detection.haar_features import evaluate_window
 from detection.sliding_window import sliding_window
+import sys
+import os
 
 #starting parameters:
 # size = 32
 # step = 8
 # threshold = -1
-def detect_faces(image, window_size=64, step=32, threshold=-10):
+def detect_faces(image, step=16, threshold=0.25):
     integral = compute_integral_image(image)
 
     detections = []
 
-    for (x, y, w, h) in sliding_window(image, window_size, step):
-        feature = haar_feature_horizontal(integral, x, y, w, h)
+    for scale in [32, 48, 64]:
+        for (x, y, w, h) in sliding_window(image, scale, step):
+            #feature = haar_feature_vertical(integral, x, y, w, h)
+            score = evaluate_window(integral, x, y, w, h)
 
-        if feature < threshold:
-            detections.append((x, y, w, h))
+            if score > threshold:
+                detections.append((x, y, w, h))
 
     return detections
 
-#if __name__ == "__main__":
-    #detections = detect_faces(image)
-
-    #print(detections)
+if __name__ == "__main__":
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # ensure root is in path
+    import webcam_debug  # run webcam_debug.py
+    sys.exit()
--- src/detection/haar_features.py
+++ src/detection/haar_features.py
@@ -1,5 +1,5 @@
-import numpy as np
-#from detection.integral_image import compute_integral_image
+import sys
+import os
 
 def rect_sum(integral, x, y, w, h):
     A = integral[y-1, x-1] if x > 0 and y > 0 else 0
@@ -9,23 +9,36 @@ def rect_sum(integral, x, y, w, h):
 
     return D - B - C + A
 
-def haar_feature_horizontal(integral, x, y, w, h):
+def haar_feature_vertical(integral, x, y, w, h):
     half_w = w // 2
 
     left_sum = rect_sum(integral, x, y, half_w, h)
     right_sum = rect_sum(integral, x + half_w, y, half_w, h)
 
-    return left_sum - right_sum
+    #return left_sum - right_sum
+    return (left_sum - right_sum) / (w * h)
 
-def is_face(feature_value, threshold = -1):
-    return feature_value < threshold
+def haar_feature_horizontal(integral, x, y, w, h):
+    half_h = h // 2
+
+    top_sum = rect_sum(integral, x, y, w, half_h)
+    bottom_sum = rect_sum(integral, x, y + half_h, w, half_h)
+
+    #return top_sum - bottom_sum
+    return (top_sum - bottom_sum) / (w * h)
+
+def evaluate_window(integral, x, y, w, h):
+    score = 0
+
+    score += haar_feature_vertical(integral, x, y, w, h)
+    score += haar_feature_horizontal(integral, x, y, w, h)
+
+    return score
+
+# def is_face(feature_value, threshold = -1):
+#     return feature_value < threshold
 
 if __name__ == "__main__":
-    img = np.array([
-    [1, 2],
-    [3, 4]
-    ], dtype=np.float32)
-
-    #integral = compute_integral_image(img)
-
-    #print(haar_feature_horizontal(integral, 0, 0, 2, 2))
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # ensure root is in path
+    import webcam_debug  # run webcam_debug.py
+    sys.exit()
