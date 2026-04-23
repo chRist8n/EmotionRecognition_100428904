e4cfc3ebad1f4114bfc312aa898a9022748b8728
chRist8n <152877601+chRist8n@users.noreply.github.com>
2026-04-23 11:18:06 +0100

    ignored files removed from git

--- src/detection/sliding_window.py
+++
@@ -1,6 +0,0 @@
-def sliding_window(image, window_size, step):
-    h, w = image.shape
-
-    for y in range(0, h - window_size + 1, step):
-        for x in range(0, w - window_size + 1, step):
-            yield (x, y, window_size, window_size)
